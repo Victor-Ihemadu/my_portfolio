e3a0963a23199158df83d32cd0149fc0a0c948e4
Apple <apple@Apples-MacBook-Air.local>
2022-02-08 20:15:20 +0100

    made changes

--- script.js
+++ script.js
@@ -33,21 +33,6 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Software Engineer", "Developer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Software Engineer", "Developer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
